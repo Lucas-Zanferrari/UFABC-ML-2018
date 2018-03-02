cb0e127adafd8e5848c15d84407bf34c0e37df34
Lucas-Zanferrari <lucas.zanferrari@gmail.com>
2018-03-02 01:00:31 -0300

    removing \src folder

--- src/ex09.py
+++ src/ex09.py
@@ -7,4 +7,5 @@ data_frame['species'] = data_frame.species.str.replace('(Iris-)', '')
 ############################################################################
 
 import seaborn as sns
-sns.boxplot(data = data_frame)
+new_data_frame = pd.melt(data_frame, id_vars = ['species'], value_name = 'size', var_name = 'measurement')
+sns.boxplot(data = new_data_frame, hue = 'species', x = 'measurement', y = 'size')
