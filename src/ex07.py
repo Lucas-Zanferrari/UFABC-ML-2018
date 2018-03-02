c9f5ec446a40293516a6672c537623b173ed859d
Lucas-Zanferrari <lucas.zanferrari@gmail.com>
2018-03-02 01:04:55 -0300

    removing unused files

--- src/ex07.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data_path = r"..\data\Iris_Data.csv"
-data_frame = pd.read_csv(data_path)
-data_frame['species'] = data_frame.species.str.replace('(Iris-)', '')
-
-############################################################################
-
-data_frame.plot.hist(alpha = 0.5).set_xlabel('Units')
